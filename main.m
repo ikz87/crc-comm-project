ce2d0d98c78d4b6064bd975450e349571f724376
ikz87 <iikz87ii@gmail.com>
2023-11-15 14:57:46 -0600

    Calculate size of output message with CRC added

--- main.m
+++ main.m
@@ -1,31 +1,45 @@
-function []=main(infile_path, outfile_path, divisor)
+function []=main(infile_path, outfile_path)
     try
+        % Divisor we'll use to get our remainders
+        divisor = [1 1 1 1];
+        divisor_bsize = numel(divisor);
+
+        % Get binary data from file
         infile  = fopen(infile_path, 'r');
         data = fread(infile, [1, Inf], 'uint8');
         fclose(infile);
         bits = uint8(rem(floor((1 ./ [128, 64, 32, 16, 8, 4, 2, 1].') * data), 2));
         bits = bits(:);
+        file_bsize = numel(bits);
 
         % We'll add a CRC after every packet
         % of n bits
-        n = 8
+        n = uint8(7);
 
         % Initialize an array with the size of
         % our (to be) coded message
+        coded_size = idivide(file_bsize, n) * divisor_bsize + file_bsize;
+        coded_rem = mod(file_bsize, n)
+        if (coded_rem > 0)
+            coded_size = coded_size + coded_rem + divisor_bsize;
+        end
+
+        out_mess = uint8.empty(0, coded_size);
         while true
-            packet = bits(1:n)
-            bits = bits(n+1:end)
+            packet = bits(1:n);
+            bits = bits(n+1:end);
 
             if numel(bits) < n 
-                % TODO
+                % TODO: Handle last packet if it has
+                % less than n bits
                 break
             end
         end
 
     catch err
-        if strcmp(err.message, 'Not enough input arguments.')
-            fprintf('Usage: main <infile> <outfile> <divisor>\n')
-        else
+%        if strcmp(err.message, 'Not enough input arguments.')
+%            fprintf('Usage: main <infile> <outfile> <divisor>\n')
+%        else
             rethrow(err);
-        end
+%        end
 end
